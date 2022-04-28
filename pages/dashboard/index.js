79cf655f80d4118929ce6ce14ae80fee67ade64c
Nayru <nanualexandraramirez@gmail.com>
2022-04-28 17:43:02 -0500

    adding vertical input to dashboard component

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -62,6 +62,15 @@ function Dashboard() {
         </div>
         <div className="mt-3">
           <input
+            type="text"
+            className="border-[1px] p-2 text-lg border-black w-full"
+            value={vertical}
+            placeholder="Vertical"
+            onChange={(e) => setVertical(e.target.value)}
+          />
+        </div>
+        <div className="mt-3">
+          <input
             type="file"
             className="border-[1px] p-2 text-lg border-black"
             onChange={(e) => setFile(e.target.files[0])}
