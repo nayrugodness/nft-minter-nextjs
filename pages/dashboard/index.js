364a7f0198092d6ecf9407da80ea6f4f1dcc7a78
Nayru <nanualexandraramirez@gmail.com>
2022-04-28 17:23:46 -0500

    adding the dashboard component

---
+++ pages/dashboard/index.js
@@ -0,0 +1,63 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useMoralis } from "react-moralis";
+import Moralis from "moralis";
+import { contractABI, contractAddress } from "../../contract";
+
+function Dashboard() {
+  const { isAuthenticated, logout, user } = useMoralis();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [file, setFile] = useState(null);
+  const router = useRouter();
+  const onSubmit = async (e) => {
+    // do something
+  };
+  useEffect(() => {
+    if (!isAuthenticated) router.replace("/");
+  }, [isAuthenticated]);
+  return (
+    <div className="flex w-screen h-screen items-center justify-center">
+      <form onSubmit={onSubmit}>
+        <div>
+          <input
+            type="text"
+            className="border-[1px] p-2 text-lg border-black w-full"
+            value={name}
+            placeholder="Name"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mt-3">
+          <input
+            type="text"
+            className="border-[1px] p-2 text-lg border-black w-full"
+            value={description}
+            placeholder="Description"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="mt-3">
+          <input
+            type="file"
+            className="border-[1px] p-2 text-lg border-black"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+        </div>
+        <button
+          type="submit"
+          className="mt-5 w-full p-5 bg-green-700 text-white text-lg rounded-xl animate-pulse"
+        >
+          Mint now!
+        </button>
+        <button
+          onClick={logout}
+          className="mt-5 w-full p-5 bg-red-700 text-white text-lg rounded-xl"
+        >
+          Logout
+        </button>
+      </form>
+    </div>
+  );
+}
+export default Dashboard;
